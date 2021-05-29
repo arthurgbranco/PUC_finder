167c8398ab281819e418a8c1b6e731a547ee854b
Arthur Branco <arthur.branco@AC-LPT-00181.local>
2021-05-29 15:53:15 -0300

    template

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import './ListenPage.dart';
+void main() => runApp(MyApp());
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) { 
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: new MyHomePage()
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  late TabController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = new TabController(vsync: this, length: 5);
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Test App"),
+        backgroundColor: Colors.teal,
+      ),
+      bottomNavigationBar: new Material(
+        color: Colors.teal,
+        child: new TabBar(
+          controller: controller,
+          tabs: <Widget>[
+            new Tab(icon: new Icon(Icons.home)),
+          ],
+        ),
+      ),
+      body: new TabBarView(controller: controller, children: <Widget>[
+        new ListenPage(),
+      ]),
+    );
+  }
+}
